c64836fbeea330c4bf45bf0e4077516af6bc2105
feildawproton <56926839+feildawproton@users.noreply.github.com>
2021-04-27 09:38:00 -0400

    Update _models.py

--- _models.py
+++ _models.py
@@ -15,9 +15,11 @@ def create_model(state_shape, kernel_dims, num_actions, filters_per_layer):
 	norm_3 	= layers.BatchNormalization()(layer_3, training = True)
 	layer_4 = layers.Conv2D(filters = filters_per_layer, kernel_size = kernel_dims, padding = "same", activation="relu", kernel_initializer = initializer)(norm_3)
 	norm_4	= layers.BatchNormalization()(layer_4, training = True)
+	layer_5 = layers.Conv2D(filters = filters_per_layer, kernel_size = kernel_dims, padding = "same", activation="relu", kernel_initializer = initializer)(norm_4)
+	norm_5	= layers.BatchNormalization()(layer_5, training = True)
 	
 	#relu because outputs are relative probabilities and predicted future rewards
-	actions = layers.Conv2D(filters = num_actions, kernel_size = (3,3), padding = "same", activation = "relu")(norm_4)
+	actions = layers.Conv2D(filters = num_actions, kernel_size = (3,3), padding = "same", activation = "relu")(norm_5)
 	
 	return keras.Model(inputs = input, outputs=actions)
 	
